26adf067a4d5c82f43c42b34cbf90a40b92edf5e
SujitChintala <saaisujith007@gmail.com>
2025-12-12 10:47:05 +0530

    importing numpy

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import os
 from PyPDF2 import PdfReader
 from werkzeug.utils import secure_filename
 import pandas as pd
+import numpy as np
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 CORS(app)
